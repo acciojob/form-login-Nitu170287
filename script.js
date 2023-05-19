b524389a21843747df1a277dbf17611ea2a3cfd3
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-05-19 23:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
-
+	
+  var first =document.getElementById("Fname").value;
+	var last =document.getElementById("Lname").value;
+  
+   alert(first+" "+last);
+  
 }
+
